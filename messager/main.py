9c1d9319a6f523ac50602c6e7b5f517a42c37960
EliteOneTube <nickhaskaris@gmail.com>
2025-01-18 16:31:45 +0200

    fix: Redirect logs to docker logs

--- messager/main.py
+++ messager/main.py
@@ -46,6 +46,8 @@ async def main():
     kafka_consumer.connect(mysql_topic)
     kafka_consumer.connect(neo4j_topic)
 
+    print(mysql_producer)
+
     # Run Producers and Consumer concurrently
     await asyncio.gather(
         mysql_producer.produce(interval=10),  # Produce MySQL data every 10 seconds

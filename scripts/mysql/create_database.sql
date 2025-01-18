362dfdfa9042fabcf23c2e4f2846e6c25cef29df
EliteOneTube <nickhaskaris@gmail.com>
2025-01-18 10:19:54 +0200

    fix: Fixed missing datatable usage in scripts

--- scripts/mysql/create_database.sql
+++ scripts/mysql/create_database.sql
@@ -4,8 +4,8 @@ USE ClothingStore;
 
 CREATE TABLE clothes (
     clothID INT AUTO_INCREMENT PRIMARY KEY,
-    style ENUM('athletic', 'casual', 'formal', 'business', 'sport', 'lounge') NOT NULL,
-    color ENUM('Red', 'Green', 'Blue', 'Black', 'White', 'Yellow', 'Purple') NOT NULL,
+    style VARCHAR(50) NOT NULL,
+    color VARCHAR(50) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     brand VARCHAR(50) NOT NULL
 );

5a0229326331c5028e983b289644c6c1eaf0db18
EliteOneTube <nickhaskaris@gmail.com>
2025-01-19 21:25:26 +0200

    fix: Neojs reading data

--- messager/sources/neo4j_source.py
+++ messager/sources/neo4j_source.py
@@ -6,17 +6,13 @@ class Neo4jDataSource(DataSource):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
     
     def fetch_data(self, rate_limit: int) -> list:
-        """Fetch data from Neo4j."""
-        session = self.driver.session()
+    """Fetch data from Neo4j."""
         query = f"""
         MATCH (n)
         RETURN n
         LIMIT {rate_limit}
         """
-        with self.driver.session() as session:
+        with self.driver.session() as session:  # Use 'with' to ensure session closure
             result = session.run(query)
-            records = [record["n"] for record in result]
-            return records
-
-        session.close()
-        return users
+            records = [record["n"] for record in result]  # Extract nodes from the query result
+        return records

860ca34355dc2e7144351dabb7393fe19d07ca6d
EliteOneTube <nickhaskaris@gmail.com>
2025-02-04 13:00:42 +0200

    feat: Run init sql script to create the user and database at startup

--- scripts/mysql/init.sql
+++ scripts/mysql/init.sql
@@ -1,17 +1,24 @@
 CREATE DATABASE IF NOT EXISTS ClothingStore;
 
--- Create a new user
-CREATE USER 'jason'@'%' IDENTIFIED BY 'ilovejason';
+-- Check if the user 'jason' exists before creating
+SET @user_exists := (SELECT COUNT(*) FROM mysql.user WHERE user = 'jason' AND host = '%');
 
--- Grant all privileges on the new database to the user
+-- Create user only if it doesn't exist
+SET @create_user_query := IF(@user_exists = 0, 'CREATE USER ''jason''@''%'' IDENTIFIED BY ''ilovejason''', 'SELECT "User already exists"');
+
+PREPARE stmt FROM @create_user_query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+
+-- Grant privileges to the user
 GRANT ALL PRIVILEGES ON ClothingStore.* TO 'jason'@'%';
 
--- Flush privileges to apply the changes
+-- Apply changes
 FLUSH PRIVILEGES;
 
 USE ClothingStore;
 
-CREATE TABLE clothes (
+CREATE TABLE IF NOT EXISTS clothes (
     clothID INT AUTO_INCREMENT PRIMARY KEY,
     style VARCHAR(50) NOT NULL,
     color VARCHAR(50) NOT NULL,

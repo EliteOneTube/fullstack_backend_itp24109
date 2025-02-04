eee651e3e68315cf960c8a25552928e463d1f5fb
EliteOneTube <nickhaskaris@gmail.com>
2025-02-04 13:23:51 +0200

    feat: Run init sql script to create the user and database at startup

--- scripts/mysql/init.sql
+++ scripts/mysql/init.sql
@@ -1,21 +1,3 @@
-CREATE DATABASE IF NOT EXISTS ClothingStore;
-
--- Check if the user 'jason' exists before creating
-SET @user_exists := (SELECT COUNT(*) FROM mysql.user WHERE user = 'jason' AND host = '%');
-
--- Create user only if it doesn't exist
-SET @create_user_query := IF(@user_exists = 0, 'CREATE USER ''jason''@''%'' IDENTIFIED BY ''ilovejason''', 'SELECT "User already exists"');
-
-PREPARE stmt FROM @create_user_query;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-
--- Grant privileges to the user
-GRANT ALL PRIVILEGES ON ClothingStore.* TO 'jason'@'%';
-
--- Apply changes
-FLUSH PRIVILEGES;
-
 USE ClothingStore;
 
 CREATE TABLE IF NOT EXISTS clothes (
